8c782a631768e9680e134c95ad0bb1675713e750
Priyandini <aandien141@gmail.com>
2024-09-25 16:21:06 +0700

    Caesar_sherley.cpp

--- Caesar_sherley.cpp
+++ Caesar_sherley.cpp
@@ -2,75 +2,75 @@
 #include <string.h>
 using namespace std;
 
-void enkripsi() {
-    string plainteks = "", cipherteks = "";
-    int i, k;
-    char c;
+void enkripsi(){
 
-    cout << "Ketikkan pesan: ";
-    cin.ignore();  // Hanya jika ada input numerik sebelumnya
-    getline(cin, plainteks);
-    do {
-        cout << "Masukkan jumlah pergeseran (0-25): ";
-        cin >> k;
-    } while (k < 0 || k > 25);
+string plainteks, cipherteks;
+int i, k;
+char c;
 
-    for (i = 0; i < plainteks.length(); i++) {
-        c = plainteks[i];
-        if (isalpha(c)) {
-            c = toupper(c);
-            c = c - 65;
-            c = (c + k) % 26;
-            c = c + 65;
-        }
-        cipherteks = cipherteks + c;
+cout << "Ketikkan pesan: ";
+cin.ignore();
+getline(cin, plainteks);
+cout << "Masukkan jumlah pergeseran (0-3): ";
+cin >> k;
+
+for(i = 0; i < plainteks.length(); i++){
+    c = plainteks[i];
+    if(isalpha(c)){
+        c = toupper(c);
+        c = c - 65;
+        c = (c + k) % 26;
+        c = c + 65;
     }
-    cout << "Cipherteks: " << cipherteks << endl;
+    cipherteks = cipherteks + c;
+}
+cout << "Cipherteks: " << cipherteks << endl;
 }
 
-void dekripsi() {
-    string plainteks = "", cipherteks = "";
-    int i, k;
-    char c;
+void dekripsi(){
+	string plainteks, cipherteks;
+int i, k;
+char c;
 
-    cout << "Ketikkan cipherteks: ";
-    cin.ignore();  // Hanya jika ada input numerik sebelumnya
-    getline(cin, cipherteks);
-    do {
-        cout << "Masukkan jumlah pergeseran (0-25): ";
-        cin >> k;
-    } while (k < 0 || k > 25);
+cout << "Ketikkan cipherteks: ";
+cin.ignore();
+getline(cin, cipherteks);
+cout << "Masukkan jumlah pergeseran (0-3): ";
+cin >> k;
 
-    for (i = 0; i < cipherteks.length(); i++) {
-        c = cipherteks[i];
-        if (isalpha(c)) {
-            c = c - 65;
-            c = (c - k + 26) % 26;
-            c = c + 65;
-        }
-        plainteks = plainteks + c;
+for(i = 0; i < cipherteks.length(); i++){
+    c = cipherteks[i];
+    if(isalpha(c)){
+    	c = toupper(c);
+        c = c - 65;
+        if (c - k < 0) {
+        	c = 26 + (c+k);
+		} else {
+			c = (c - k) % 26;
+			c = c + 65;
+			c = tolower(c);
+		}
     }
-    cout << "Plainteks: " << plainteks << endl;
+    plainteks = plainteks + c;
+}
+cout << "Plainteks: " << plainteks << endl;
 }
 
 int main() {
-    int pil;
-    bool stop = false;
-
-    while (!stop) {
-        cout << "Menu " << endl;
-        cout << "1. enkripsi " << endl;
-        cout << "2. dekripsi " << endl;
-        cout << "3. exit " << endl;
-        cout << "pilih menu "; 
-        cin >> pil;
-        cin.ignore();  // Bersihkan buffer setelah input numerik
-
-        switch (pil) {
-            case 1: enkripsi(); break;
-            case 2: dekripsi(); break;
-            case 3: stop = true; break;
-            default: cout << "Pilihan tidak valid!" << endl; break;
-        }
+int pil; 
+bool stop;
+stop = false;
+while(!stop){
+    cout << "Menu " << endl;
+    cout << "1. enkripsi " << endl;
+    cout << "2. dekripsi " << endl;
+    cout << "3. exit " << endl;
+    
+    cout << "pilih menu "; cin >> pil;
+    switch(pil){
+        case 1: enkripsi(); break;
+        case 2: dekripsi(); break;
+        case 3: stop = true; break;
     }
+}
 }
